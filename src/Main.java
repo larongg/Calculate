0bd0ab68160e3c5db97e45a2aedc40c520e62f79
larongg <shestikpetr@gmail.com>
2022-04-21 18:49:52 +0800

    Проблемы с римской сорок

--- src/Main.java
+++ src/Main.java
@@ -25,11 +25,35 @@ public class Main {
         if (s.length != 3) {return "throws Exception";}
 // римские
         if (rim_true(s[0]) && rim_true(s[2])) {
+            String rim_str = "";
             byte rim_cif = Byte.parseByte(calculation_arab(new String[]{Integer.toString((int)s[0].charAt(0)-8495-'0'), s[1], Integer.toString((int)s[2].charAt(0)-8495-'0')}));
+
             if (rim_cif < 1) {return "throws Exception";}
-            if (rim_cif > 10) {
-                return (Character.toString((char)8553) + ((char)(rim_cif-10+'0'+8495)));
+
+            if (rim_cif >= 10 && rim_cif < 40) {
+                for (byte i=0;i<rim_cif/10;i++) {rim_str += (char)8553;}
+                if (rim_cif % 10 != 0) {rim_str += (char)(rim_cif % 10 + '0' + 8495);}
             }
+
+            if (rim_cif >= 40 && rim_cif < 50) {
+                rim_str += (char)8553 + (char)8556;
+                if (rim_cif % 10 != 0) {rim_str += (char)(rim_cif % 10 + '0' + 8495);}
+            }
+
+            if (rim_cif >= 50 && rim_cif < 90) {
+                rim_str += (char)8556;
+                for (byte i=0;i<(rim_cif-50)/10;i++) {rim_str += (char)8553;}
+                if (rim_cif % 10 != 0) {rim_str += (char)(rim_cif % 10 + '0' + 8495);}
+            }
+            if (rim_cif >= 90 && rim_cif < 100) {
+                rim_str += (char)8553 + (char)8557;
+                if (rim_cif % 10 != 0) {rim_str += (char)(rim_cif % 10 + '0' + 8495);}
+            }
+            if (rim_cif == 100) {rim_str += (char)8557;}
+
+            if (rim_cif >= 1 && rim_cif < 10) {rim_str += (char)(rim_cif + '0' + 8495);}
+
+            return rim_str;
         }
 // арабские
         if (arab_true(s[0]) && arab_true(s[2])) {return calculation_arab(s);}
@@ -41,9 +65,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
         System.out.println(calc(s));
+
         //System.out.println(((int)'Ⅰ' - (int)'1'));
         //System.out.println(((char)((int)'Ⅴ'-8495))-'0');
         //System.out.println((char)(5+2+'0'+8495));
+        //Integer.toString((int)h.charAt(0)-8495-'0');
+
+        /*char[] rim = {8544,8545,8546,8547,8548,8549,8550,8551,8552,8553};
+        for (char i:rim){
+            for (char j:rim){
+                String s = "";
+                s += i + " * " + j;
+                System.out.print(calc(s) + " ");
+            }
+            System.out.println();
+        }*/
     }
 }
-//Integer.toString((int)h.charAt(0)-8495-'0')
